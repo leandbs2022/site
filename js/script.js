401e158cb9e3834d5f7415d5630f6c91cd5a3b76
Leandro BSB <109764459+leandbs2022@users.noreply.github.com>
2022-10-05 18:52:08 -0300

    051020221852

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-
-function recuperar(){
+function recuperar() {
   /* if(document.getElementById("cPadrao").checked == true){ let valor = "1"
    ['<? =$selecionado= ?>'] = valor
 }
@@ -9,47 +8,48 @@ function recuperar(){
    if(document.getElementById("cadmin").checked == true){let valor = "3"
 
 }*/
-window.Location.href('recuperar.php')
- }
-
-
-
+  window.Location.href("recuperar.php");
+}
 
 function toggleFullScreen() {
-  if ((document.fullScreenElement && document.fullScreenElement !== null) ||    
-   (!document.mozFullScreen && !document.webkitIsFullScreen)) {
-    if (document.documentElement.requestFullScreen) {  
-      document.documentElement.requestFullScreen();  
-    } else if (document.documentElement.mozRequestFullScreen) {  
-      document.documentElement.mozRequestFullScreen();  
-    } else if (document.documentElement.webkitRequestFullScreen) {  
-      document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);  
-    }  
-  } else {  
-    if (document.cancelFullScreen) {  
-      document.cancelFullScreen();  
-    } else if (document.mozCancelFullScreen) {  
-      document.mozCancelFullScreen();  
-    } else if (document.webkitCancelFullScreen) {  
-      document.webkitCancelFullScreen();  
-    }  
-  }  
-} 
+  if (
+    (document.fullScreenElement && document.fullScreenElement !== null) ||
+    (!document.mozFullScreen && !document.webkitIsFullScreen)
+  ) {
+    if (document.documentElement.requestFullScreen) {
+      document.documentElement.requestFullScreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+      document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullScreen) {
+      document.documentElement.webkitRequestFullScreen(
+        Element.ALLOW_KEYBOARD_INPUT
+      );
+    }
+  } else {
+    if (document.cancelFullScreen) {
+      document.cancelFullScreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitCancelFullScreen) {
+      document.webkitCancelFullScreen();
+    }
+  }
+}
 function requestFullScreen() {
   var el = document.body;
 
   // Suporta a maioria dos navegadores e suas versões.
-  var requestMethod = el.requestFullScreen || el.webkitRequestFullScreen 
-  || el.mozRequestFullScreen || el.msRequestFullScreen;
+  var requestMethod =
+    el.requestFullScreen ||
+    el.webkitRequestFullScreen ||
+    el.mozRequestFullScreen ||
+    el.msRequestFullScreen;
 
   if (requestMethod) {
-
-   // Tela cheia nativa.
+    // Tela cheia nativa.
     requestMethod.call(el);
-
   } else if (typeof window.ActiveXObject !== "undefined") {
-
-// IE mais antigo.
+    // IE mais antigo.
     var wscript = new ActiveXObject("WScript.Shell");
 
     if (wscript !== null) {
@@ -60,25 +60,22 @@ function requestFullScreen() {
 function frame(pages) {
   switch (pages) {
     case 0:
-      document.getElementById('telas').src = 'dashbord.php'
+      document.getElementById("telas").src = "dashbord.php";
       break;
     case 1:
-      document.getElementById('telas').src = 'clientes.php'
+      document.getElementById("telas").src = "clientes.php";
       break;
     case 2:
-      document.getElementById('telas').src = 'funcionarios.php'
+      document.getElementById("telas").src = "funcionarios.php";
       break;
     case 3:
-      document.getElementById('telas').src = 'produtos.php'
+      document.getElementById("telas").src = "produtos.php";
       break;
     case 4:
-      document.getElementById('telas').src = 'fornecedor.php'
+      document.getElementById("telas").src = "fornecedor.php";
       break;
     case 5:
-      document.getElementById('telas').src = 'usuarios.php'
-      break;
-      case 6:
-      document.getElementById('telas').src = 'relatorio1.php'
+      document.getElementById("telas").src = "usuarios.php";
       break;
     default:
       break;
@@ -86,67 +83,53 @@ function frame(pages) {
 }
 
 function pagina(relatorios) {
-
-  window.location.href = 'relatorios.php'
-   
+  window.location.href = "relatorios.php";
 }
 function graficoPizza() {
-
   var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
   var yValues = [55, 49, 44, 24, 15];
-  var barColors = [
-    "#b91d47",
-    "#00aba9",
-    "#2b5797",
-    "#e8c3b9",
-    "#1e7145"
-  ];
+  var barColors = ["#b91d47", "#00aba9", "#2b5797", "#e8c3b9", "#1e7145"];
 
   new Chart("myChart", {
     type: "pie",
     data: {
       labels: xValues,
-      datasets: [{
-        backgroundColor: barColors,
-        data: yValues
-      }]
+      datasets: [
+        {
+          backgroundColor: barColors,
+          data: yValues,
+        },
+      ],
     },
     options: {
       title: {
         display: true,
-        text: "World Wide Wine Production 2018"
-      }
-    }
+        text: "World Wide Wine Production 2018",
+      },
+    },
   });
-
 }
 function graficoRosca() {
-
   var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
   var yValues = [55, 49, 44, 24, 15];
-  var barColors = [
-    "#b91d47",
-    "#00aba9",
-    "#2b5797",
-    "#e8c3b9",
-    "#1e7145"
-  ];
+  var barColors = ["#b91d47", "#00aba9", "#2b5797", "#e8c3b9", "#1e7145"];
 
   new Chart("myChart", {
     type: "doughnut",
     data: {
       labels: xValues,
-      datasets: [{
-        backgroundColor: barColors,
-        data: yValues
-      }]
+      datasets: [
+        {
+          backgroundColor: barColors,
+          data: yValues,
+        },
+      ],
     },
     options: {
       title: {
         display: true,
-        text: "World Wide Wine Production 2018"
-      }
-    }
+        text: "World Wide Wine Production 2018",
+      },
+    },
   });
-
 }
